7e4c5b62e8638099a13cfeade207195b4a7ca6fd
Samuel Cochran <sj26@sj26.com>
2023-10-30 09:20:57 +0100

    wip

--- workspaces/libnpmpublish/lib/provenance.js
+++ workspaces/libnpmpublish/lib/provenance.js
@@ -9,6 +9,9 @@ const INTOTO_STATEMENT_V1_TYPE = 'https://in-toto.io/Statement/v1'
 const SLSA_PREDICATE_V02_TYPE = 'https://slsa.dev/provenance/v0.2'
 const SLSA_PREDICATE_V1_TYPE = 'https://slsa.dev/provenance/v1'
 
+const BUILDKITE_BUILDER_ID_PREFIX = 'https://buildkite.com/agents'
+const BUILDKITE_BUILD_TYPE = 'https://buildkite.github.io/slsa-buildtypes/job/v1'
+
 const GITHUB_BUILDER_ID_PREFIX = 'https://github.com/actions/runner'
 const GITHUB_BUILD_TYPE = 'https://slsa-framework.github.io/github-actions-buildtypes/workflow/v1'
 
@@ -17,6 +20,51 @@ const GITLAB_BUILD_TYPE_VERSION = 'v0alpha1'
 
 const generateProvenance = async (subject, opts) => {
   let payload
+  if (ci.BUILDKITE) {
+    /* istanbul ignore next - not covering missing env var case */
+    const [workflowPath, workflowRef] = (env.GITHUB_WORKFLOW_REF || '')
+      .replace(env.GITHUB_REPOSITORY + '/', '')
+      .split('@')
+    payload = {
+      _type: INTOTO_STATEMENT_V1_TYPE,
+      subject,
+      predicateType: SLSA_PREDICATE_V1_TYPE,
+      predicate: {
+        buildDefinition: {
+          buildType: BUILDKITE_BUILD_TYPE,
+          externalParameters: {
+            workflow: {
+              ref: workflowRef,
+              repository: `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}`,
+              path: workflowPath,
+            },
+          },
+          internalParameters: {
+            github: {
+              event_name: env.GITHUB_EVENT_NAME,
+              repository_id: env.GITHUB_REPOSITORY_ID,
+              repository_owner_id: env.GITHUB_REPOSITORY_OWNER_ID,
+            },
+          },
+          resolvedDependencies: [
+            {
+              uri: `git+${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}@${env.GITHUB_REF}`,
+              digest: {
+                gitCommit: env.GITHUB_SHA,
+              },
+            },
+          ],
+        },
+        runDetails: {
+          builder: { id: `${GITHUB_BUILDER_ID_PREFIX}/${env.RUNNER_ENVIRONMENT}` },
+          metadata: {
+            /* eslint-disable-next-line max-len */
+            invocationId: `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}/actions/runs/${env.GITHUB_RUN_ID}/attempts/${env.GITHUB_RUN_ATTEMPT}`,
+          },
+        },
+      },
+    }
+  }
   if (ci.GITHUB_ACTIONS) {
     /* istanbul ignore next - not covering missing env var case */
     const [workflowPath, workflowRef] = (env.GITHUB_WORKFLOW_REF || '')
